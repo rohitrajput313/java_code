520ee8b3dd92a2f010e5c842ea86eb072467e39b
rohitrajput313 <rohitrajput313@gmail.com>
2023-12-23 15:15:13 +0530

    second commit

--- ifelsif.java
+++ ifelsif.java
@@ -1,13 +1,13 @@
 public class ifelsif {
     public static void main(String[] args) {
         String city = "Delhi";
-        if(city=="Meerut"){
+        if (city == "Meerut") {
             System.out.println("city is Meerut");
-        }else if(city=="Noida"){
+        } else if (city == "Noida") {
             System.out.println("city is Noida");
-        }else if(city=="Agra"){
-            System.out.println("city is Agra");
-        }else{
+        } else if (city == "Agra") {
+            System.out.println("city is Agra has");
+        } else {
             System.out.println(city);
         }
     }
